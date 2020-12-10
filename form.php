ad94321d34103c7401081c3fdf289a68b7397b2a
Anil bist <anilbist48@gmail.com>
2020-12-10 08:41:26 +0545

    inital

---
+++ form.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="login ">
+		<h1> Admin Login </h1>
+	<form action= "form.php" method="post">
+		<div class="textbox">
+		<input type="text" name = "username" placeholder="Username" required>
+		</div>
+		<div class="textbox">
+		<input type="password" name = "password" placeholder="Password" required>
+		</div>
+		<input type="submit" name ="submit" value ="Login" class="btn">
+	</form>	
+	</div>
+</body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,50 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Product</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header id="header">
+		<div class ="container">
+			<h1>Product Management</h1>
+		</div>
+	</header>
+		<nav id= "bar">
+			<div class ="container" >
+				<ul>
+					<li><a href ="">Home</a></li>
+					<li><a href ="">Products</a></li>
+					<li><a href ="">About Us</a></li>
+					<li><a href ="form.php">Account</a></li>
+				</ul>
+			</div>
+		</nav>
+		<div class ="container" >
+	<div class="image">
+  		<a target="_blank" href="producti/anil.jpg">
+    		<img src="producti/anil.jpg"  width="600" height="400">
+  		</a>
+  			<div class="desc">Add a description of the image here</div>
+	</div>
+
+	<div class="image">
+		<a target="_blank" href="producti/book.jpg">
+		    <img src="producti/book.jpg"  width="600" height="400">
+		</a>
+	  		<div class="desc">Add a description of the image here</div>
+	</div>
+
+	<div class="image">
+	  	<a target="_blank" href="producti/laptop.jpg">
+	    	<img src="producti/laptop.jpg"  width="600" height="400">
+	  	</a>
+	  		<div class="desc">Add a description of the image here</div>
+	</div>
+</div>
+
+</body>
+</html>
